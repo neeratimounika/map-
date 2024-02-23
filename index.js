ed224663c7635222bc07d73190a490ea31e024f1
mounika.neerati <mounika.neerati93@gmail.com>
2024-02-23 16:56:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var birthYears =[2008,1993,1986,1990,1989,2020] 
+
+var ages = birthYears.map(function(ele,index,arr){
+    let age = 2023-ele 
+    return age;
+})
+console.log(birthYears)
+console.log(ages)
