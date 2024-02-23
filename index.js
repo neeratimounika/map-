035dd636497b02478da1a367a006977817b549cd
mounika.neerati <mounika.neerati93@gmail.com>
2024-02-23 17:39:11 +0530

    from filter_fnctn

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var birthYears =[2008,1993,1986,1990,1989,2020] 
+// var birthYears =[2008,1993,1986,1990,1989,2020] 
 
-var ages = birthYears.map(function(ele,index,arr){
-    let age = 2023-ele 
-    return age;
-})
-console.log(birthYears)
-console.log(ages)
+// var ages = birthYears.map(function(ele,index,arr){
+//     let age = 2023-ele 
+//     return age;
+// })
+// console.log(birthYears)
+// console.log(ages)
+
+var arr = [1,2,3,45,6]
+
+var maxValue = arr.reduce(function(acc,ele){
+    if(acc>ele){
+        return acc
+    }else{
+        return ele
+    }
+
+},arr[0])
+
+console.log(maxValue)
